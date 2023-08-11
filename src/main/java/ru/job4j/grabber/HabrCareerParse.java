2012521b1d3fff8ede44f4b822ab9f96916319f8
Eugeny_ch <evch@rambler.ru>
2023-08-11 22:55:14 +1000

    2.4. HabrCareerParse [#285213 # [#285213]] Changed NUMBER_OF_PAGES from 1 to 5

--- src/main/java/ru/job4j/grabber/HabrCareerParse.java
+++ src/main/java/ru/job4j/grabber/HabrCareerParse.java
@@ -15,7 +15,7 @@ import java.util.List;
 
 
 public class HabrCareerParse implements Parse {
-    private static final int NUMBER_OF_PAGES = 1;
+    private static final int NUMBER_OF_PAGES = 5;
     private static final String SOURCE_LINK = "https://career.habr.com";
     private static final String PAGE_LINK = String.format("%s/vacancies/java_developer?page=", SOURCE_LINK);
     private final DateTimeParser dateTimeParser;
